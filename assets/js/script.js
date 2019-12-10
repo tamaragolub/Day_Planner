3a35dbd420ae7aa2df999624e51eb33c88a9b41a
Tamara Golub <tgolub89@gmail.com>
2019-12-10 12:52:43 -0500

    completed the javascript code needed to run the application

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,14 @@ $(".save-btn").on("click", function(){
 })
 
 
-console.log(localStorage.getItem("#9am-text"))
+var textIDs = ["#9am-text", "#10am-text", "#11am-text", "#12pm-text", "#1pm-text", "#2pm-text", "#3pm-text", "#4pm-text", "#5pm-text"]
 
 // for loop to get the texts for various IDs
+
+for (var i = 0; i < textIDs.length; i++){
+    
+   var result = localStorage.getItem(textIDs[i])
+
+    $(textIDs[i]).val(result);
+}
 
